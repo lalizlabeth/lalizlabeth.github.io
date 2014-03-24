8be30a843063afb7c5ee6ede1e353a61e42eb04a
Elizabeth Lin <elizabethylin@gmail.com>
2014-03-24 14:42:26 -0700

    fixing js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -24,18 +24,14 @@ $(document).ready(function() {
 
   $(window).scroll(function() {
     var scrollHeight = $(document).scrollTop();
-    if (firstScroll === false) {
-      toggleNavigation("open");
-      firstScroll = true;
+    if (scrollHeight === 0) {
+      toggleNavigation("closed");
+      firstScroll = false;
+    } else {
+      if (firstScroll === false) {
+        toggleNavigation("open");
+        firstScroll = true;
+      }
     }
-    // if (scrollHeight === 0) {
-    //   toggleNavigation("closed");
-    //   firstScroll = false;
-    // } else {
-    //   if (firstScroll === false) {
-    //     toggleNavigation("open");
-    //     firstScroll = true;
-    //   }
-    // }
   });
 });
