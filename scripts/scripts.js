977b7df57e8f07ff390153f36e1fd48ca12226d4
Elizabeth Lin <elizabethylin@gmail.com>
2014-03-30 09:34:28 -0700

    fixing stuf

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,3 @@
-$body = $("body");
-$(document).on({
-    ajaxStart: function() { $body.addClass("loading");    },
-     ajaxStop: function() { $body.removeClass("loading"); }    
-});
-
 $(document).ready(function() {
   var firstScroll = false;
 
