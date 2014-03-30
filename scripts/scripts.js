2db6fcd53067ed8c158e48179a04118a9b7d9768
Elizabeth Lin <elizabethylin@gmail.com>
2014-03-30 09:31:29 -0700

    testing loading'

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,9 @@
+$body = $("body");
+$(document).on({
+    ajaxStart: function() { $body.addClass("loading");    },
+     ajaxStop: function() { $body.removeClass("loading"); }    
+});
+
 $(document).ready(function() {
   var firstScroll = false;
 
