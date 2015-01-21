0ebe6c63c54a5cb5fc658fdaf3c404bb7a7813d6
Elizabeth <elizabethylin@gmail.com>
2015-01-21 10:46:54 -0800

    remeedy test

---
+++ remedy/index.php
@@ -0,0 +1,347 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href='//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="/css/animate.css">
+	<link rel="stylesheet" type="text/css" href="/css/styles.css">
+	<meta name="viewport" content="user-scalable=0, initial-scale=1.0">
+</head>
+<body>
+
+<div id='nav' class='navbar intro'>
+	<div class='ditto-wmargin'>
+		<div class='logo'><img style='height: 40px; float: left' src='/img/remedy-logo-cropped.png' /></div>
+		<div id='menu' class='menu'>
+			<a class='active' href="/">Home</a>
+			<a href="/mission.html">Mission</a>
+			<a href="/join-us.html">Join Us</a>
+		</div>
+	</div>
+</div>
+
+<div id='main' class='header'>
+	<div>
+		<div class="vcenter textcenter">
+
+			<table style="margin: auto"><tr>
+
+			<td class='postpadding' style="width: 415px">
+				<div class='welcome'>
+					<h1>On-demand specialist consults</h1>
+					<p>Instant clinician to clinician consults</p>
+				</div>
+			</td>
+			<td style="width: 40px"></td>
+			<td>
+				<div class='iphone-wmask iphone-video'>
+					<img class='center' height="600" src="/img/big_iphone@2x.png">
+					<div class='iphone-mask'>
+						<video id="movie" preload='auto' autoplay loop src="video/doctortalking_low.mp4" poster='/img/video_poster@2x.png'/>
+					</div>
+					<div class='iphone-border'>
+					</div>
+				</div>
+			</td>
+
+			</tr></table>
+
+		</div>
+	</div>
+</div>
+
+
+<section class='bg-main'>
+
+	<form class='textcenter lightform apply ditto-wmargin'>
+		<div>
+			<h3>Redefine the future of care delivery</h3>
+			<input type='text' placeholder='Full Name' required>
+			<input type='text' placeholder='Email Address' required>
+			<select>
+				<option value="none">Specialty...</option>
+				<option>ENT</option>
+				<option>Dermatology</option>
+				<option>Gynecology</option>
+				<option>Hand Surgery</option>
+				<option>Orthopedic Surgery</option>
+				<option>Psychiatry</option>
+			</select>
+			<input type='button' value='Apply' style='margin-top: 20px'>
+		</div>
+	</form>
+
+</section>
+
+<div class='bg-white'>
+
+<section class='ditto-wmargin'>
+
+	<div class='product-section'>
+
+		<div class='title textcenter'>
+			<h2 style='margin-top: 0px'>Remedy means <b>rapid</b> resolutions</h2>
+		</div>
+
+		<table style="margin: 50px 0px"><tr>
+		<td width='300'>
+			<div class='iphone-wmask'>
+				<img class='center' height="600" src="/img/big_iphone@2x.png">
+				<div class='iphone-mask'>
+
+					<img src='/img/demo/Specialty1 2.png'>
+
+					<img src='/img/demo/Specialty1.png'>
+
+					<!--
+
+					<div class='wow fadeIn'
+						data-wow-duration='0.25s'
+						data-wow-delay='5s'
+						style='background: white'>
+
+						<img class='wow fadeInUp'
+						data-wow-duration='1s'
+						data-wow-delay='6s'
+						src='/img/demo/chatdemo1.png'>
+
+						<img class='wow fadeInUp'
+						data-wow-duration='1s'
+						data-wow-delay='7s'
+						src='/img/demo/chatdemo2.png'>
+
+						<img class='wow fadeInUp'
+						data-wow-duration='1s'
+						data-wow-delay='8.5s'
+						src='/img/demo/chatdemo3.png'>
+
+						<img src='/img/demo/chatdemobg.png'>
+
+					</div>
+
+					-->
+
+				</div>
+				<div class='iphone-border'>
+				</div>
+			</div>
+		</td>
+		<td width='40'></td>
+		<td>
+
+			<h3><strong>Instantly</strong> consult with experienced specialists.</h3>
+		<p>Board certified<br>Fellowship trained<br>USA based</p>
+
+		<h3><strong>One tool<!-- to rule them all--></strong> resolves the simplest to the most complex concerns.</h3>
+		<p>Secure Chat<br>Rich Multimedia<br>Voice and Video Calls</p>
+
+		</td>
+		</tr></table>
+
+		<table style='margin: 50px 0px'><tr>
+		<td>
+
+		<h3><strong>Seamlessly</strong> refer patients when concerns can’t be resolved virtually.</h3>
+
+		<h3><strong>HIPAA Compliant</strong><br />256 AES encryption secures sensitive patient information across all devices.</h3>
+
+		</td>
+		<td width='40'></td>
+		<td width='300'>
+			<div class='iphone-wmask'>
+				<img class='center' height="600" src="/img/big_iphone@2x.png">
+				<div class='iphone-mask'>
+
+					<img src='/img/demo/appointmentdemo.png'>
+
+				</div>
+				<div class='iphone-border'>
+				</div>
+			</div>
+		</td>
+		</tr></table>
+
+	</div>
+
+</section>
+
+<section class='bg-main'>
+	<div class='ditto-wmargin'>
+		<table><tr>
+		<td width='312'>
+			<img src='/img/glass-nobg@2x.png' width='312'/>
+		</td>
+		<td width='40'></td>
+		<td>
+			<h3>Google Glass Integration</h3>
+			<p style='font-size: 20px'>Send photos and videos taken from Glass to a specialist hands-free.</p>
+		</td>
+		</tr></table>
+	</div>
+</section>
+
+<section class='ditto-wmargin'>
+
+	<h2 class='title textcenter'>Consult with World Class Specialists</h2>
+
+	<table><tr><td>
+	<h3>Dr. Warren Strudwick consults and schedules through remedy</h3>
+
+	<p>Dr. Strudwick is an orthopaedic surgeon specializing in the treatment of injuries to the knee, elbow and shoulder.</p>
+
+	<p>Dr. Strudwick is the team physician and orthopaedic surgeon for the Oakland Raiders. He was formerly the team physician for the U.S.A Track & Field Team, the Golden State Warriors, and the U.S.A Olympic Boxing Team. Dr. Strudwick is a member of the American Academy of Orthopaedic Surgeons, the American College of Sports Medicine, and the National Football League Physicians Association.</p>
+
+	<p>Dr. Strudwick completed his residency in orthopedic surgery from Stanford University.</p>
+	</td>
+	<td style='width: 50px'></td>
+	<td>
+		<img src='/img/warren.png' style='height: 415px'>
+	</td>
+	</tr></table>
+
+</section>
+
+<section class='bg-main'>
+	<div class='ditto-wmargin'>
+		<div class='quote'>
+			<p class='quote-text'>"Remedy brings the highest levels of specialist expertise to a much broader set of patients in real time"</p>
+			<div class='person-bio'>
+				<div class='person-bio-desc'>
+					<p><span class='person-name'>Mark Foran</span>, MD, MPH</p>
+                    <p>Medical Director of Health Information Technology</p>
+      				<p>Faculty, NYU Global Institute of Public Health</p>
+      			</div>
+  			</div>
+		</div>
+	</div>
+</section>
+
+<section id='press' class='ditto-wmargin textcenter'>
+	<div class='title'>
+		<h2>Press</h2>
+	</div>
+	<div class='press-array'>
+
+		<a target='_blank' class='press-item' href='http://mashable.com/2014/08/19/google-glass-patients-remedy/' style='background-image: url(/img/press/screen-mashable.jpg)'>
+			<div class='press-item-overlay' style='background-color: #00aeef'></div>
+			<img class='source' src='/img/press/mashable-logo-white.png'>
+		</a>
+
+		<a target='_blank' class='press-item' href='http://techcrunch.com/2014/08/25/remedy/' style='background-image: url(/img/press/screen-tc.jpg)'>
+			<div class='press-item-overlay' style='background-color: #0A9E01'></div>
+			<img class='source' src='/img/press/tc-logo-white.png'>
+		</a>
+
+		<a target='_blank' class='press-item' href='http://www.fastcoexist.com/3028555/change-generation/a-google-glass-app-for-doctors-to-stream-video-of-patients-to-consult-othe' style='background-image: url(/img/press/screen-fastcompany.jpg)'>
+			<div class='press-item-overlay' style='background-color: black'></div>
+			<img class='source' src='/img/press/FastCompany-logo-white.svg'>
+		</a>
+
+		<a target='_blank' class='press-item' href='http://finance.yahoo.com/news/remedy-shows-patients-see-experts-143000507.html' style='background-image: url(/img/press/screen-yahoo.jpg)'>
+			<div class='press-item-overlay' style='background-color: #500095'></div>
+			<img class='source' src='/img/press/yahoo-logo-white.png'>
+		</a>
+
+		<a target='_blank' class='press-item' href='http://techpageone.co.uk/en/business/entrepreneurship-business/startup-offers-remedy-inefficient-health-care/' style='background-image: url(/img/press/screen-dell.jpg)'>
+			<div class='press-item-overlay' style='background-color: #1484C7'></div>
+			<img class='source' src='/img/press/dell-logo-white.png' style='max-height: 100px'>
+		</a>
+
+		<a target='_blank' class='press-item' href='http://mobihealthnews.com/33025/three-harvard-hospitals-pilot-google-glass-app-remedy/' style='background-image: url(/img/press/screen-mobihealth.jpg)'>
+			<div class='press-item-overlay' style='background-color: #20794D'></div>
+			<img class='source' src='/img/press/mhn-logo-white.png' style='max-height: 70px'>
+		</a>
+
+    </div>
+
+</section><!-- end of section -->
+
+<section class='bg-main'>
+	<div class='ditto-wmargin'>
+		<div class='quote'>
+			<p class='quote-text'>"Remedy dramatically reduces the time patients spend waiting for specialists"</p>
+			<div class='person-bio'>
+				<div class='person-bio-desc'>
+					<p><span class='person-name'>Dr. Steven Schwaitzberg</span>, MD</p>
+					<p>Chief of Surgery, Harvard University</p>
+				</div>	
+			</div>
+		</div>
+	</div>
+</section>
+
+<section>
+
+	<div id='solution' class='textcenter'>
+
+		<div class='features'>
+
+		<div class='feature'>
+			<div class='feature-img'><i class="fa fa-tablet"></i></div>
+			<h2>Anywhere</h2>
+			<p class='feature-desc'>Android, iOS, and Google Glass for a hands-free experience. </p>
+		</div>
+
+		<div class='feature' data-wow-delay='0.5s'>
+			<div class='feature-img'><i class="fa fa-user-md"></i></div>
+			<h2>Effortless</h2>
+			<p class='feature-desc'>Immediate responses from the best specialists in the country.</p>
+		</div>
+
+		<div class='feature' data-wow-delay='1s'>
+			<div class='feature-img'><i class="fa fa-shield"></i></div>
+			<h2>Protected</h2>
+			<p class='feature-desc'>HIPAA compliant, industry standard encryption.</p>
+		</div>
+
+		</div><!-- end of features -->
+
+	</div>
+
+</section>
+
+</div><!-- end of bg-white -->
+
+<section class='section-wbg' style='background-color: #098EEC'>
+	
+	<h3 class='textcenter' style='margin: 0px'>The new standard of care is here.</h3>
+
+</section>
+<section class='bg-main'>
+
+	<div>
+		<div class='ditto-wmargin textcenter'>
+			<form class='textcenter lightform'class="apply ditto">
+			<h3>Inquire for details on pricing</h3>
+			<input type='text' placeholder='Full Name' required>
+			<input type='text' placeholder='Email Address' required>
+			<select>
+				<option value="none">Specialty...</option>
+				<option>ENT</option>
+				<option>Dermatology</option>
+				<option>Gynecology</option>
+				<option>Hand Surgery</option>
+				<option>Orthopedic Surgery</option>
+				<option>Psychiatry</option>
+			</select>
+			<input type='button' value='Apply' style='margin-top: 20px'>
+		</form>
+		</div>
+	</div>
+</section>
+
+<div class='footer'>
+	<div class='ditto-wmargin'>
+		<div class='logo' style='margin: 60px 0px; float: left;'><img style='height: 40px; float: left' src='/img/remedy-logo-cropped.png' /><!--<p class='copyright'>© 2014 Remedy Inc.</p>--></div>
+		<h3 style='margin-top: 60px; float: right; line-height: 40px'>Read more about Remedy in our <a style='font-size: inherit'>blog</a></h3>
+		
+	</div>
+</div>
+
+<script src='/js/wow.min.js'></script>
+<script>
+	new WOW().init();
+</script>
+<script src='/js/home.js'></script>
+
+</body>
+</html>
